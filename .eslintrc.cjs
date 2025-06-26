6ccbf93336467276fb2c8f5b954066557ddac0f1
Josechap <joschapa@gmail.com>
2025-06-26 15:43:13 -0600

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,49 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: [
+      "./tsconfig.json",
+    ],
+  },
+  plugins: [
+    "@typescript-eslint",
+    "react",
+    "react-hooks",
+    "unused-imports",
+    "tailwindcss",
+    "prettier",
+  ],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:tailwindcss/recommended",
+    "plugin:prettier/recommended",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      typescript: {},
+    },
+  },
+  rules: {
+    "prettier/prettier": ["error"],
+    "react/react-in-jsx-scope": "off", // Not needed with React 17+
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "unused-imports/no-unused-imports": "error",
+  },
+};
